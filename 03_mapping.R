a0e135b075294da6b440349050681fc16a39f664
amkessler <kessler.aaron@gmail.com>
2019-09-09 16:29:54 -0400

    add merged kml file for precincts

--- 03_mapping.R
+++ 03_mapping.R
@@ -1,6 +1,22 @@
+# https://r-spatial.github.io/sf/articles/sf1.html
+
 library(sf)
 
-anson <- st_read("geo_data/doc.kml")
+anson <- st_read("geo_data/anson.kml")
 
-plot(anson)
 plot(anson[1])
+
+
+union <- st_read("geo_data/union.kml")
+
+plot(union[1])
+
+
+
+
+merged <- st_read("geo_data/mergedKML_NC09precincts.kml")
+
+plot(merged)
+plot(merged[3])
+
+
