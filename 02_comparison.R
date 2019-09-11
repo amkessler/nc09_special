94cdcc553d80c0b88e2ddb4ef629743cf830953b
Aaron Kessler <kessler.aaron@gmail.com>
2019-09-10 23:11:00 -0400

    add prez results from '16

--- 02_comparison.R
+++ 02_comparison.R
@@ -37,3 +37,37 @@ midterm_2018 <- midterm_2018 %>%
     gop18pct = round_half_up((gop18/total18)*100, 1),
     margin18 = abs(dem18pct - gop18pct)
   )
+
+
+
+# presidential from 2016 #### -------------------------
+
+nc2016_prez <- readRDS("processed_data/nc2016_prez.rds")
+
+names(nc2016_prez)
+
+prez_2016 <- nc2016_prez %>% 
+  select(county, precinct, choice_party, total_votes)
+
+#reshape to get candidate votes going across
+prez_2016 <- prez_2016 %>% 
+  dcast(county + precinct ~ choice_party, value.var = "total_votes", sum) %>% 
+  as_tibble()
+
+#rename columns
+prez_2016 <- prez_2016 %>% 
+  rename(
+    dem16 = DEM,
+    lib16 = LIB,
+    gop16 = REP,
+    non16 = NON
+  )
+
+#calculate total and percentages for each candidate
+prez_2016 <- prez_2016 %>% 
+  mutate(
+    total16 = (dem16 + lib16 + gop16 + non16),
+    dem16pct = round_half_up((dem16/total16)*100, 1),
+    gop16pct = round_half_up((gop16/total16)*100, 1),
+    margin16 = abs(dem16pct - gop16pct)
+  )
