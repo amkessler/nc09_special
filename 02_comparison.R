d0802b1a56cf68ea7a82b24a2009d01cdd9303f2
amkessler <kessler.aaron@gmail.com>
2019-09-11 13:48:53 -0400

    create derived fields for analysis

--- 02_comparison.R
+++ 02_comparison.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(janitor)
 library(lubridate)
 library(reshape2)
+options(scipen = 999)
 
 #load 2018 results from step 00
 nc2018_house9 <- readRDS("processed_data/nc2018_house9.rds")
@@ -76,7 +77,7 @@ special_2019 <- special_2019 %>%
     total19 = (dem19 + lib19 + gop19 + gre19),
     dem19pct = round_half_up((dem19/total19)*100, 1),
     gop19pct = round_half_up((gop19/total19)*100, 1),
-    margin19 = abs(dem18pct - gop19pct)
+    margin19 = abs(dem19pct - gop19pct)
   )
 
 #save result to file
@@ -87,7 +88,33 @@ saveRDS(special_2019, "processed_data/special_2019.rds")
 
 ### Join together and compare #### ----------------------------------
 
-joined <- inner_join(special_2019, midterm_2018)
+joined_allcols <- inner_join(midterm_2018, special_2019)
 
+joined_allcols 
 
+names(joined_allcols)
 
+#select just the columns needed for the pct comparisons
+joined <- joined_allcols %>% 
+  select(
+    county,
+    precinct,
+    dem18pct,
+    gop18pct,
+    margin18,
+    dem19pct,
+    gop19pct,
+    margin19
+  )
+
+#create derived columns to use for analysis
+joined <- joined %>% 
+  mutate(
+    winner18 = if_else(dem18pct > gop18pct, "D", "R"),
+    winner19 = if_else(dem19pct > gop19pct, "D", "R"),
+    flip = if_else(winner18 == winner19, "N", "Y"),
+    dem_change = dem19pct - dem18pct,
+    gop_change = gop19pct - gop18pct,
+    margin_tot_change = round_half_up(margin19 - margin18, 1)
+  )
+  
